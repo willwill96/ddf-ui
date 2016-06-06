2c48cf86b7615f6d83dadd1f29823b342c49e2af
Ryan Zwiefelhofer <ryan.zwiefelhofer@gmail.com>
2016-06-06 15:02:36 -0700

    Spark endpoint conversion

--- catalog/ui/catalog-ui-search/src/main/webapp/component/metacard-history/metacard-history.view.js
+++ catalog/ui/catalog-ui-search/src/main/webapp/component/metacard-history/metacard-history.view.js
@@ -52,7 +52,7 @@ define([
             var loadingView = new LoadingView();
             var self = this;
             setTimeout(function(){
-                $.get('/services/search/catalog/history/'+self.model.get('metacard').get('id')).then(function(response){
+                $.get('/search/catalog/internal/history/'+self.model.get('metacard').get('id')).then(function(response){
                     self._history = response;
                 }).always(function(){
                     loadingView.remove();
@@ -92,7 +92,7 @@ define([
         revertToSelectedVersion: function(){
             var loadingView = new LoadingView();
             var self = this;
-            $.get('/services/search/catalog/history/'+this.model.get('metacard').get('id')+'/revert/'+selectedVersion).then(function(response){
+            $.get('/search/catalog/internal/history/'+this.model.get('metacard').get('id')+'/revert/'+selectedVersion).then(function(response){
                 self.model.get('metacard').get('properties').set(response.metacards[0]);
             }).always(function(){
                 setTimeout(function(){  //let solr flush
